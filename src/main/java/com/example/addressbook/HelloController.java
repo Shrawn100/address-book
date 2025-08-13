98a8b1e457c5bce32b67e4538263af585ef5fe1c
Shrawn <shrawn2002@gmail.com>
2025-08-13 19:14:01 +1000

    Include Address Book terms and conditions

--- src/main/java/com/example/addressbook/HelloController.java
+++ src/main/java/com/example/addressbook/HelloController.java
@@ -2,13 +2,30 @@ package com.example.addressbook;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.*;
 
 public class HelloController {
     @FXML
     private Label welcomeText;
 
     @FXML
+    private TextArea termsAndConditions;
+
+    @FXML public void initialize(){
+        termsAndConditions.setText("""
+                LOREM IPSUM DOLOR LOREM IPSUM DOLOR LOREM IPSUM DOLOR LOREM IPSUM DOLOR
+                LOREM IPSUM DOLOR LOREM IPSUM DOLOR LOREM IPSUM DOLOR LOREM IPSUM DOLOR
+                LOREM IPSUM DOLOR LOREM IPSUM DOLOR LOREM IPSUM DOLOR LOREM IPSUM DOLOR
+                LOREM IPSUM DOLOR LOREM IPSUM DOLOR LOREM IPSUM DOLOR LOREM IPSUM DOLOR
+                LOREM IPSUM DOLOR LOREM IPSUM DOLOR LOREM IPSUM DOLOR LOREM IPSUM DOLOR
+                LOREM IPSUM DOLOR LOREM IPSUM DOLOR LOREM IPSUM DOLOR LOREM IPSUM DOLOR
+                LOREM IPSUM DOLOR LOREM IPSUM DOLOR LOREM IPSUM DOLOR LOREM IPSUM DOLOR
+                """);
+    }
+
+    @FXML
     protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+        welcomeText.setText("Welcome to the Address Book Application!");
     }
 }
