7a19b2cebd8cc3156230361b33a3b8c6dc08697f
Shrawn <shrawn2002@gmail.com>
2025-08-13 19:55:52 +1000

    Implement transition into main screen

--- src/main/java/com/example/addressbook/HelloController.java
+++ src/main/java/com/example/addressbook/HelloController.java
@@ -1,9 +1,14 @@
 package com.example.addressbook;
 
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.*;
+import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class HelloController {
     @FXML
@@ -28,4 +33,29 @@ public class HelloController {
     protected void onHelloButtonClick() {
         welcomeText.setText("Welcome to the Address Book Application!");
     }
+
+
+    @FXML private CheckBox agreeCheckBox;
+
+    @FXML private Button nextButton;
+
+    @FXML
+    protected void onAgreeCheckBoxClick(){
+        boolean accepted= agreeCheckBox.isSelected();
+
+        nextButton.setDisable(!accepted);
+    }
+
+    @FXML protected void onNextButtonClick()throws IOException {
+        Stage stage= (Stage) nextButton.getScene().getWindow();
+
+        FXMLLoader fxmlLoader= new FXMLLoader(HelloApplication.class.getResource("main-view.fxml"));
+
+        Scene scene = new Scene(fxmlLoader.load(),640,360);
+        stage.setScene(scene);
+    }
+
+
+
+
 }
